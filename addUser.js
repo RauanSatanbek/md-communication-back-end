daf1e6113cf85e0427cb327b8903e37812e0c943
Rauan Satanbek <rauan.satanbek@gmail.com>
2016-11-22 10:00:33 +0600

    dsad

--- addUser.js
+++ addUser.js
@@ -3,3 +3,9 @@ var Users = require("./app/models/user.js").Users;
 Users.find({}, function(err, result) {
 	console.log(result);
 });
+
+var user = new Users({
+	name: String,
+	avatar: String,
+	posts: String
+})
